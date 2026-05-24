310c06a0423490218547b7fa3282a0d4f958cade
Steven Arenas <stevena.arenasr@uqvirtual.edu.co>
2026-05-24 09:31:06 -0500

    Agregar Interface INotificable

---
+++ src/main/java/co/edu/uniquindio/poo/tech_park_uq/controller/modell/interfaces/INotificable.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.poo.tech_park_uq.controller.modell.interfaces;
+
+import co.edu.uniquindio.poo.techparkuq.model.records.Notificacion;
+
+public interface INotificable {
+
+    void recibirNotificacion(Notificacion notificacion);
+}
