295fc5f2d8a28c2c1816fbb6b3f1c94c37e34203
jhzz-8 <juanj.juradoc@uqvirtual.edu.co>
2026-05-24 08:57:29 -0500

    Agregando clases enums, TipoNotificacion

---
+++ src/main/java/co/edu/uniquindio/poo/tech_park_uq/controller/modell/interfaces/TipoNotificacion.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.poo.tech_park_uq.controller.modell.interfaces;
+
+
+public enum TipoNotificacion {
+    SHOW,
+    CLIMA,
+    MANTENIMIENTO,
+    ALERTA,
+    INFORMACION
+}
