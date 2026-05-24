3f5047787c271fffa8c91328dbd93249406a3310
Steven Arenas <stevena.arenasr@uqvirtual.edu.co>
2026-05-24 09:33:21 -0500

    Agregar Record registroAcceso

---
+++ src/main/java/co/edu/uniquindio/poo/tech_park_uq/controller/modell/records/RegistroAcceso.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.poo.tech_park_uq.controller.modell.records;
+
+import co.edu.uniquindio.poo.techparkuq.model.enums.EstadoAcceso;
+
+import java.time.LocalDateTime;
+
+public record RegistroAcceso(
+    String nombreAtraccion,
+    EstadoAcceso estado,
+    String motivo,
+    LocalDateTime fechaHora,
+    int puntosUsados
+) {
+}
